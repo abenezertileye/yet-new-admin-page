bdb5127ed66541ed37b4c0f921aae65079c68e38
abene <abenezertileye2003@gmail.com>
2025-01-02 23:26:48 +0300

    integration improved

---
+++ yet-new-admin-page/src/app/api/apiSlice.js
@@ -0,0 +1,80 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  reducerPath: 'api', // Unique name for the API slice
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3000',
+    prepareHeaders: (headers, { getState }) => {
+      // Get token from localStorage or Redux state
+      const token = localStorage.getItem('token');  // Or use getState() to fetch from Redux store
+
+      // If token exists, add it as a Bearer token to the Authorization header
+      if (token) {
+        console.log('token found')
+        headers.set('Authorization', `Bearer ${token}`);
+      } else {
+        console.log(token)
+        return
+      }
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSchedules: builder.query({
+      query: () => '/admin/get-all-schedule',
+    }),
+    getDrivers: builder.query({
+      query: () => '/admin/get-all-drivers',
+    }),
+    getHelpers: builder.query({
+      query: () => '/admin/get-all-helpers',
+    }),
+    getBuses: builder.query({
+      query: () => '/admin/get-all-busses',
+    }),
+    getRoutes: builder.query({
+      query: () => '/admin/get-all-routes',
+    }),
+    login: builder.mutation({
+      query: (loginParameters) => ({
+        url: '/admin-auth/login',
+        method: 'POST',
+        body: loginParameters,
+      }),
+    }),
+    createSchedule: builder.mutation({
+      query: (createSchedule) => ({
+        url: '/admin/create-schedule',
+        method: 'POST',
+        body: createSchedule,
+      }),
+    }),
+    createDriver: builder.mutation({
+      query: (DriverData) => ({
+        url: '/driver/signup',
+        method: 'POST',
+        body: DriverData,
+      }),
+    }),
+    createHelper: builder.mutation({
+      query: (HelperData) => ({
+        url: '/helper/signup',
+        method: 'POST',
+        body: HelperData,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useCreateScheduleMutation,
+  useCreateDriverMutation,
+  useCreateHelperMutation,
+  useGetSchedulesQuery,
+  useGetDriversQuery,
+  useGetHelpersQuery,
+  useGetBusesQuery,
+  useGetRoutesQuery,
+} = apiSlice; // Export hooks
