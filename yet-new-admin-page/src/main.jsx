c65c97fd0d00ecec0b4fa60e1daba168c85f8c6f
abene <abenezertileye2003@gmail.com>
2025-01-02 23:26:37 +0300

    integration improved

--- yet-new-admin-page/src/main.jsx
+++ yet-new-admin-page/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import { Provider } from "react-redux"; // Import Redux Provider
+import App from "./App"; // Import the App component
+import { store } from "./app/store"; // Import the Redux store
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}> {/* Provide the Redux store */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
